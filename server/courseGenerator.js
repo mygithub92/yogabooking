cebb28e104ff21c6504c69831ec65ddf074118f4
David <linlwangster@gmail.com>
2017-05-19 18:30:59 +0930

    changesss

--- server/courseGenerator.js
+++ server/courseGenerator.js
@@ -20,9 +20,9 @@ var dayInMillisecond = 86400000;
 		if(validDays.indexOf(dow) > -1){
 			for (i = 0; i < validPeriods[dow].length; i++) {
 				var period = validPeriods[dow][i];
-				var newCourse = {date:current.format('YYYY-MM-DD'),start_time:period.start,end_time:period.end,spot_number:period.spotNumber,coachId:coachId,addressId:addressId}
+				var newCourse = {course_date:current.format('YYYY-MM-DD'),start_time:period.start,end_time:period.end,spot_number:period.spotNumber,coachId:coachId,addressId:addressId}
                 
-                db.create(db.course,{date: newCourse.date,start_time:newCourse.start_time,end_time:newCourse.end_time},newCourse,function(){
+                db.create(db.course,{course_date: '2017-05-19',start_time:newCourse.start_time,end_time:newCourse.end_time},newCourse,function(){
                     console.log("**********************************");
                     console.log(newCourse);
                     console.log(" has been created");
--- server/module/course.js
+++ server/module/course.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
   var Course = sequelize.define('course', {
    id: {type:DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    date:DataTypes.DATE,
+    course_date:DataTypes.DATE,
     start_time: DataTypes.TIME,
     end_time:DataTypes.TIME,
     spot_number:DataTypes.INTEGER
